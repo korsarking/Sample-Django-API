6a25aeb004aa2aaf1ad3044cab7c0fe408bcec8a
Artiom Cozlovici <kozakovi4@outlook.com>
2025-06-20 12:33:18 +0300

    feat: Add REST_FRAMEWORK settings for authentication configuration

--- src/main/settings.py
+++ src/main/settings.py
@@ -121,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
