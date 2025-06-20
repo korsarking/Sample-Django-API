2094b662a58a2dc3eb22dadde387ca18e3bd7660
Artiom Cozlovici <kozakovi4@outlook.com>
2025-06-20 15:03:44 +0300

    feat: Integrate Dynaconf for dynamic configuration management

--- src/main/settings.py
+++ src/main/settings.py
@@ -128,3 +128,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
 }
+
+# HERE STARTS DYNACONF EXTENSION LOAD (Keep at the very bottom of settings.py)
+# Read more at https://www.dynaconf.com/django/
+import dynaconf  # noqa
+settings = dynaconf.DjangoDynaconf(__name__)  # noqa
+# HERE ENDS DYNACONF EXTENSION LOAD (No more code below this line)
