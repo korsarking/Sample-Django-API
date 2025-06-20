1eb3a05a1c1c6003673fc608dc208efac1abd4cf
Artiom Cozlovici <kozakovi4@outlook.com>
2025-06-20 15:54:58 +0300

    fix: Update SECRET_KEY to use environment variable for production security

--- src/main/settings.py
+++ src/main/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m^hmm7#n=wbjuc8v8c19&nye50i-l3$=7cb&geihw^uas_!*in'
+# Secret key should be set in environment variable or dynaconf settings.
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
