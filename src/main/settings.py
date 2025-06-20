bfec5349a93d19d67997bb3874f1d369deb13b3a
Artiom Cozlovici <kozakovi4@outlook.com>
2025-06-20 15:55:07 +0300

    chore: Update database configuration to use environment variables or dynaconf settings

--- src/main/settings.py
+++ src/main/settings.py
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # Database configuration should be set in environment variables or dynaconf settings.
 }
 
 
